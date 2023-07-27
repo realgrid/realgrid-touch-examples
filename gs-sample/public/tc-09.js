b86d9b366de923fafcfc82d909d88de901285153
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2023-07-27 17:20:56 +0900

    tc9

---
+++ gs-sample/public/tc-09.js
@@ -0,0 +1,104 @@
+var data;
+var list;
+
+
+const footer_template = {
+  template: {
+    layout: "hlinear",
+    children: [{ 
+        value: "${@row_count} 개",
+        style: { fontWeight: "bold", color: "#555" }
+      }
+    ],
+  },
+};
+
+
+const row_template = {
+  template: {
+      layout: 'vlinear',
+      children: [{
+          layout: 'hlinear',
+          children: [{
+              field: 'SIGUN_NM',
+              style: {
+                  fontSize: '17px',
+                  fontWeight: 'bold',
+                  color: '#555'
+              },
+          }, {
+              field: 'DIV',
+              style: {
+                  fontSize: '15px',
+                  color: '#55f'
+              },
+          }]
+      }, {
+          layout: 'hlinear',
+          width: '100%',
+          children: [{
+              field: 'ENTRPS_NM',
+              style: {
+                  fontSize: '15px',
+                  fontWeight: 'bold',
+                  color: '#555'
+              },
+          }, {
+              field: 'PRODLST_NM',
+              style: {
+                  fontSize: '14px',
+                  color: '#777'
+              }
+          }, {
+              space: '*'
+          }, {
+              field: 'TELNO',
+              style: {
+                  fontSize: '14px',
+                  color: '#797'
+              }
+          }]
+      }]
+  },
+};
+
+const config = {
+  props: { 
+    templates: { // 템플릿 등록
+      row: row_template, 
+      footer: footer_template
+    },
+  },
+  options: {
+    row: {  // 데이터행 설정 모델
+      template: "row", // 데이터행 레이아웃 생성에 사용되는 템플릿 이름 등록
+    },
+    footer: { // 푸터 설정 모델
+      template: "footer" // 푸터 레이아웃 생성에 사용된느 템플릿 이름 등록
+    },
+  },
+};
+
+async function createListData(dataurl, callback) {
+  try {
+    const res = await fetch(dataurl);
+    const json = await res.json();
+
+    data = RealTouch.createListData("", {}, { values: json })
+      .createView('dataview',  {})
+      .sort(['SIGUN_NM']) // 그룹핑 기준으로 정렬
+      .build();
+    callback && callback(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function init() {
+  createListData("./data/group.json", (data) => {
+    list = RealTouch.createListControl(document, "realtouch");
+    list.setConfig(config);
+    list.data = data;
+    list.rowGroupBy(['SIGUN_NM']); // '시군'필드를 기준으로 행그룹핑 설정
+  });
+}

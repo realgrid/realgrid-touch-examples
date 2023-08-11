d98cc411da8237109ac8e7eacd40f8c21af554b7
benny-yoo <123144980+benny-yoo@users.noreply.github.com>
2023-08-11 10:41:21 +0900

    TTA 공인 아이피 대역

--- gs-sample/public/lib/realgrid-touch-lic.js
+++ gs-sample/public/lib/realgrid-touch-lic.js
@@ -1,2 +1,2 @@
-// *.realgrid.com,*.localhost,localhost,127.0.0.1,192.168.*,172.16.*,10.*
-var realTouchLic = 'upVcPE+wPOlJsRH3nb2OcoJrINF5KSa8kFJ3gVa7eMKFmEUEwiRZWJ96d5Xv0pv1i/KcE100Cv5pns7Dj5LHfwFYXvd+bEGsydV5ApQckONbbLvWO2T7CyOxVpb7v1suS8beBVzF89K3SfqUdIFpBF7svsLh1nO8CmamnnCmQbuhVJPLWiHNuJjecSIpjOml';
+// *.realgrid.com,*.localhost,localhost,127.0.0.1,192.168.*,172.16.*,10.*,210.104.181.*
+var realTouchLic = 'upVcPE+wPOlJsRH3nb2OcoJrINF5KSa8kFJ3gVa7eMKFmEUEwiRZWJ96d5Xv0pv1i/KcE100Cv5pns7Dj5LHfwFYXvd+bEGsydV5ApQckONbbLvWO2T7CyOxVpb7v1suS8beBVzF89K3SfqUdIFpBOf6fSXJxL3+inhtRQX9UnsO5+/xJSE3LoSxnPX5CHZn0QeIbtyyIEGTFVqWmNPdyQ==';
